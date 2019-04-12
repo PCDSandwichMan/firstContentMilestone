a3770285f6b9a6697915aab0c7381fc0310c3eaf
Matt <matthew.endicott@snhu.edu>
2019-04-11 19:10:50 -0600

    Still trying to impletment the navbar dropdown menus I am having issues with not having them load initialy when the page loads.

--- mainFile/scripts/script.js
+++ mainFile/scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
-    $('.nav-item').on('click', (event) =>{
-        $(event.currentTarget).siblings('.dropdown-menu').toggle();
-    });
-
-    
+  $(".nav-item").on("click", event => {
+    $(event.currentTarget)
+      .siblings(".dropdown-menu")
+      .toggle();
+  });
 });
